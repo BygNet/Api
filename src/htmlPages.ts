7d1565bc2c13c555f0c05be1153c4f2ac43bb568
ash <ash@asboy2035.com>
2026-01-14 10:21:04 +0100

    update URL for Byg

--- src/htmlPages.ts
+++ src/htmlPages.ts
@@ -23,11 +23,13 @@ export const HomePage: string = `
 <html lang="en">
   <head>
     <title>Byg API</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     ${styles}
   </head>
   <body>
     <h1>Byg API home.</h1>
-    <a href="https://big.a35.dev">Byg App for Web.</a>
+    <a href="https://byg.a35.dev">Byg App for Web.</a>
   </body>
 </html>
 `
