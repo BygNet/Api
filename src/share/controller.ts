7aa3816fe3566f5fc793407c22e96fe58d8478f8
ash <ash@a35.dev>
2026-03-09 18:01:52 +0100

    fix share link creation

--- src/share/controller.ts
+++ src/share/controller.ts
@@ -1,7 +1,7 @@
 import { ShareQueries } from '@/share/queries'
 
 function constructShareUrl(id: number, path: string): string {
-  return `https://${import.meta.env.BASE_URL}/${path}/${id}`
+  return `${import.meta.env.BASE_URL}/${path}/${id}`
 }
 
 export abstract class ShareController {
